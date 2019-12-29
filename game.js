907778bf22c141d8c69a92299901513f66ac639a
Chris Wood <16053164+ChrisWoody@users.noreply.github.com>
2019-12-29 11:35:12 +0800

    Major update, the game works now, a lot of improvements that could be added though

--- game.js
+++ game.js
@@ -21,6 +21,30 @@ let mouseOverRed = false;
 let mouseOverBlue = false;
 let mouseOverYellow = false;
 
+let highlightGreen = false;
+let highlightRed = false;
+let highlightBlue = false;
+let highlightYellow = false;
+
+let gameStarted = false;
+
+let date = new Date();
+let previous = date.getTime();
+let now = date.getTime();
+let delta = now - previous;
+
+const DelayBetween = 500;
+const ShowTime = 1000;
+let currentDelayBetween = 0;
+let currentShowTime = 0;
+
+let showingSquare = false;
+let showingSequence = false;
+let gameSequenceIndex = -1; // could be used to capture both 'showing' and 'player' sequence
+let gameSequence = [];
+let totalSequenceCount = 1; // so number of squares to show in the sequence
+let gameOver = false;
+
 document.onmousemove = function (e) {
     mouseX = (window.Event) ? e.pageX : e.clientX + (document.documentElement.scrollLeft ? document.documentElement.scrollLeft : document.body.scrollLeft);
     mouseY = (window.Event) ? e.pageY : e.clientY + (document.documentElement.scrollTop ? document.documentElement.scrollTop : document.body.scrollTop);
@@ -34,27 +58,142 @@ document.onmousemove = function (e) {
     mouseOverYellow = mouseX <= -buffer && mouseX >= -squareLengthBuffer && mouseY >= buffer && mouseY <= squareLengthBuffer;
 }
 
-function draw() {
+document.onmousedown = function (e) {
 
-    // background
+    if (showingSequence) {
+        // showing the sequence of squares to the player, ignore their input
+    } else if (gameOver) {
+        // start a new game
+        totalSequenceCount = 1;
+        gameSequence = [];
+        gameOver = false;
+        gameSequenceIndex = -1;
+        showingSequence = true;
+    } else if (gameStarted) {
+        // game is underway but we're not showing the sequence, check they have picked the correct square
+
+        // crude way of handling this
+        if (gameSequenceIndex == -1) {
+            gameSequenceIndex = 0;
+        }
+
+        let currentSquare = gameSequence[gameSequenceIndex];
+        let pickedCorrectSquare = false;
+        switch (currentSquare) {
+            case 1: pickedCorrectSquare = mouseOverGreen; break;
+            case 2: pickedCorrectSquare = mouseOverRed; break;
+            case 3: pickedCorrectSquare = mouseOverBlue; break;
+            case 4: pickedCorrectSquare = mouseOverYellow; break;
+
+            default: break;
+        }
+
+        if (pickedCorrectSquare) {
+            gameSequenceIndex++;
+            if (gameSequenceIndex >= gameSequence.length) {
+                // they've completed the sequence, show it again with a new square added to the end
+                totalSequenceCount++;
+                showingSequence = true;
+                gameSequenceIndex = -1;
+            }
+        } else {
+            gameOver = true;
+        }
+
+    } else {
+        gameStarted = true;
+        showingSequence = true;
+    }
+}
+
+function drawBackground() {
     context.fillStyle = 'rgb(200, 200, 200)';
     context.fillRect(-(width / 2), -(height / 2), width, height);
+}
 
-    // green top left
-    context.fillStyle = mouseOverGreen ? 'rgb(0, 255, 0, 1.0)' : 'rgb(0, 255, 0, 0.8)';
+function drawGreen() {
+    context.fillStyle = highlightGreen ? 'rgb(0, 255, 0, 1.0)' : 'rgb(0, 255, 0, 0.5)';
     context.fillRect(-squareLengthBuffer, -squareLengthBuffer, squareLength, squareLength);
+}
 
-    // red top right
-    context.fillStyle = mouseOverRed ? 'rgb(255, 0, 0, 1.0)' : 'rgb(255, 0, 0, 0.8)';
+function drawRed() {
+    context.fillStyle = highlightRed ? 'rgb(255, 0, 0, 1.0)' : 'rgb(255, 0, 0, 0.5)';
     context.fillRect(buffer, -squareLengthBuffer, squareLength, squareLength)
+}
 
-    // blue bottom right
-    context.fillStyle = mouseOverBlue ? 'rgb(0, 0, 255, 1.0)' : 'rgb(0, 0, 255, 0.8)';
+function drawBlue() {
+    context.fillStyle = highlightBlue ? 'rgb(0, 0, 255, 1.0)' : 'rgb(0, 0, 255, 0.5)';
     context.fillRect(buffer, buffer, squareLength, squareLength);
+}
 
-    // yellow bottom left
-    context.fillStyle = mouseOverYellow ? 'rgb(255, 255, 0, 1.0)' : 'rgb(255, 255, 0, 0.8)';
-    context.fillRect(-squareLengthBuffer, buffer, squareLength, squareLength)
+function drawYellow() {
+    context.fillStyle = highlightYellow ? 'rgb(255, 255, 0, 1.0)' : 'rgb(255, 255, 0, 0.5)';
+    context.fillRect(-squareLengthBuffer, buffer, squareLength, squareLength);
+}
+
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function clearHighlights() {
+    highlightGreen = highlightRed = highlightBlue = highlightYellow = false;
+}
+
+function draw() {
+    date = new Date();
+    now = date.getTime();
+    delta = now - previous;
+    previous = now;
+
+    drawBackground();
+    clearHighlights();
+
+    if (gameStarted) {
+        if (gameOver) {
+            highlightGreen = highlightRed = highlightBlue = highlightYellow = true;
+            // should also print out the score, ie the number of i guess successful squares, so the current total count - 1
+        } else if (showingSequence) {
+            if (showingSquare) {
+                currentShowTime += delta;
+                if (currentShowTime >= ShowTime) {
+                    showingSquare = false;
+                    currentShowTime = 0;
+
+                    // shown the entire sequence? if so stop showing it then wait for user to play
+                    if (gameSequenceIndex + 1 >= gameSequence.length) {
+                        showingSequence = false;
+                        gameSequenceIndex = -1;
+                    }
+
+                } else {
+                    let currentSquare = gameSequence[gameSequenceIndex];
+                    switch (currentSquare) {
+                        case 1: highlightGreen = true; break;
+                        case 2: highlightRed = true; break;
+                        case 3: highlightBlue = true; break;
+                        case 4: highlightYellow = true; break;
+
+                        default: break;
+                    }
+                }
+            } else { // some small setup before we show a sequence BUT also increments the current square
+                // could have a delay between showing squares here
+                if (gameSequence.length < totalSequenceCount) {
+                    gameSequence.push(getRndInteger(1, 4));
+                }
+
+                gameSequenceIndex++;
+                showingSquare = true;
+            }
+        } else {
+            // waiting for user input
+        }
+    }
+
+    drawGreen();
+    drawRed();
+    drawBlue();
+    drawYellow();
 
     requestAnimationFrame(draw);
 }
